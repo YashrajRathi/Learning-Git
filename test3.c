63de4d1fa19c117a37eae452ef11af29dca35a69
yashraj <yashraj11rathi@gmail.com>
2021-04-25 23:34:25 +0530

    One file added into another branch of system .

---
+++ test3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+void main(){
+
+	int n;
+	printf("Enter any number that you want to find square for :\n");
+	scanf("%d",&n);
+	printf("The square of given number is %d \n",n*n);
+
+}
