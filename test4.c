86bf5855a42172bf055a394740a7e506f4c9e3e0
yashraj <yashraj11rathi@gmail.com>
2021-04-26 15:00:20 +0530

    Added threshold warning to sample program.

--- test4.c
+++ test4.c
@@ -7,6 +7,10 @@ void main(){
 	printf("Enter the number that you want to find factorial.\n");
 	int n = 0;
 	scanf("%d",&n);
+	if(n>=40){
+		printf("The number is above our threshold limit.Please enter a smaller number.\n");
+		return;
+	}
 	unsigned long fact = 1;
 	for(int i=2;i<=n;i++){
 		fact = fact * i;
